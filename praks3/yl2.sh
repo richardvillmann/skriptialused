9d3a3db3ae438e48cc11b765c488fa33727eb51a
richardiso20 <richard.villmann@khk.ee>
2020-11-19 09:26:12 +0200

    Kersti kaljulaid

---
+++ praks3/yl2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###Lause formuleerimine
+kes=" Kersti Kaljulaid"
+kuna="Aastal 1999-2002"
+mistegi=" majandusnõunik peaminister Mart Laarile"
+oli=" oli"
+lause=$kuna$oli$kes$mistegi
+echo
+echo
+echo $lause
+echo
