0cfeb7eca1e027d10516b9ceadf559e95fbe5088
richardiso20 <richard.villmann@khk.ee>
2020-11-19 09:08:02 +0200

    praktikum3 nime väljastamine

---
+++ praks3/yl1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+###skript mis küsib  kasutajalt nime ja väljastab selle
+echo
+echo
+echo -n "sisesta oma ees- ja perenimi:"
+read eesnimi perenimi
+echo "Sinu eesnimi on $eesnimi ja perenimi on $perenimi"
