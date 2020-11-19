9fe1200900dcd394bc8834fdcbe2fdaa29f8656a
richardiso20 <richard.villmann@khk.ee>
2020-11-19 16:34:11 +0200

    arvu kontrollimine algarvuna

---
+++ praks6/yl4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+###Skript, mis tuvastab, kas arv on algarv voi mitte
+echo
+echo
+echo -n "Palun sisestage üks arv: "
+read arv
+if [ $arv = 0 ] || [ $arv = 1 ]; then
+	echo "$arv ei ole algarv"
+else
+	jagaja=2
+	while (( $arv%$jagaja!=0 )); do ###liidan tehte argumenti
+###Kui kasutan () sulge, võin sinna panna tehte, kui kasutan [] sulge, pean kirjutama tehte sõnaga
+		jagaja=$(($jagaja + 1))
+	done
+	if [ $arv -eq $jagaja ]; then
+		echo "$arv on algarv"
+	else
+		echo "$arv ei ole algarv"
+	fi
+fi
