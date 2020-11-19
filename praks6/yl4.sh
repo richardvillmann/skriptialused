a2a39f910aa6f076f4ad6dbff2a319ee0703c698
richardiso20 <richard.villmann@khk.ee>
2020-11-19 16:47:24 +0200

    kontrollib kas arv on algarv või mitte ja välistab numbrid yle 20

--- praks6/yl4.sh
+++ praks6/yl4.sh
@@ -4,11 +4,13 @@ echo
 echo
 echo -n "Palun sisestage üks arv: "
 read arv
-if [ $arv = 0 ] || [ $arv = 1 ]; then
+if [ $arv = 0 ] || [ $arv = 1 ] && [ $arv -le 20 ]; then
 	echo "$arv ei ole algarv"
+elif [ $arv -gt 20 ]; then
+	echo "Ei kontrolli nii suuri arve, Edastan andmed politseisse"
 else
 	jagaja=2
-	while (( $arv%$jagaja!=0 )); do ###liidan tehte argumenti
+	while (( $arv%$jagaja!=0 )) && [ $arv -le 20 ]; do ###liidan tehte argumenti
 ###Kui kasutan () sulge, võin sinna panna tehte, kui kasutan [] sulge, pean kirjutama tehte sõnaga
 		jagaja=$(($jagaja + 1))
 	done
