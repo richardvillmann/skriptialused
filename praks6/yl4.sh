06218220c6843c2d112b749560237fb522ab740f
richardiso20 <richard.villmann@khk.ee>
2020-11-19 16:56:58 +0200

    väljastab 1-20 numbrid ja ütleb kas need on algarvud või ei ole

--- praks6/yl4.sh
+++ praks6/yl4.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 ###Skript, mis tuvastab, kas arv on algarv voi mitte
-echo
-echo
-echo -n "Palun sisestage üks arv: "
-read arv
-if [ $arv = 0 ] || [ $arv = 1 ] && [ $arv -le 20 ]; then
-	echo "$arv ei ole algarv"
-elif [ $arv -gt 20 ]; then
-	echo "Ei kontrolli nii suuri arve, Edastan andmed politseisse"
-else
-	jagaja=2
-	while (( $arv%$jagaja!=0 )) && [ $arv -le 20 ]; do ###liidan tehte argumenti
+#echo
+#echo
+#echo -n "Palun sisestage üks arv: "
+#read arv
+#if [ $arv = 0 ] || [ $arv = 1 ] && [ $arv -le 20 ]; then
+#	echo "$arv ei ole algarv"
+#elif [ $arv -gt 20 ]; then
+#	echo "Ei kontrolli nii suuri arve, Edastan andmed politseisse"
+#else
+#	jagaja=2
+#	while (( $arv%$jagaja!=0 )) && [ $arv -le 20 ]; do ###liidan tehte argumenti
 ###Kui kasutan () sulge, võin sinna panna tehte, kui kasutan [] sulge, pean kirjutama tehte sõnaga
-		jagaja=$(($jagaja + 1))
-	done
-	if [ $arv -eq $jagaja ]; then
-		echo "$arv on algarv"
+#		jagaja=$(($jagaja + 1))
+#	done
+#	if [ $arv -eq $jagaja ]; then
+#		echo "$arv on algarv"
+#	else
+#		echo "$arv ei ole algarv"
+#	fi
+#fi
+echo
+echo
+for (( arv=1; arv<=20; arv++)); do
+	if [ $arv = 1 ]; then
+	       echo "$arv ei ole algarv"
 	else
-		echo "$arv ei ole algarv"
+		jagaja=2
+      		while (( $arv%$jagaja!=0 )); do
+	               jagaja=$(($jagaja + 1))
+		done
+		if [ $arv -eq $jagaja ]; then
+			echo "$arv on algarv"
+      		else
+	        	echo "$arv ei ole algarv"
+		fi
 	fi
-fi
+done
