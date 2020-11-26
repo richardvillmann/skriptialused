790a6f0b7c809e438f6621fcb94ca2d5f78d7f9c
richardiso20 <richard.villmann@khk.ee>
2020-11-26 14:05:07 +0200

    püramiidi ehitamine

---
+++ praks7/yl3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#**********Skript, millega saab ridade kaudu püramiidi ehitada********
+echo
+echo -n "Sisesta ridade arv:"
+read rida
+for ((i=1; i<=$rida; i++))
+do
+echo -n  "$i."
+	for ((j=1; j<=i; j++)); do
+	echo -n  "*"
+	done
+echo ""
+done
